b5c722f13045e27d66f8da92489a68c87880e1f7
ioskpu <lrcorales@gmail.com>
2024-08-28 07:56:49 -0500

    Se agrega encapsulamiento: Getters and setters

--- POO/src/App.java
+++ POO/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //instanciar un objeto persona
-        Persona persona1 = new Persona("Luis", "Corales", 50, "programnación");
+        Persona persona1 = new Persona("Luis", "Corales", 50);
         //imprimir el nombre completo de la persona
-        System.out.println(persona1.darNombreCompleto() + " tiene " + persona1.edad + " años y esta graduada en " + persona1.carrera.nombre);
+        System.out.println(persona1.darNombreCompleto() + " tiene " + persona1.getEdad() + " años");
     }
 }
--- POO/src/Persona.java
+++ POO/src/Persona.java
@@ -1,35 +1,52 @@
 public class Persona {
     // Atributos (estado/caracteristicas de un objeto)
-    String nombre;
-    String apellido;
-    int edad;    
-    //boolean esEstudiante;
-    Carrera carrera;
+    private String nombre;
+    private String apellido;
+    private int edad;    
+    
 
     //constructor
-    public Persona(String nombre, String apellido, int edad, String nombreCarrera, int duracion, boolean estaEnCurso){
-        carrera = new Carrera(nombreCarrera, duracion, estaEnCurso);
+    public Persona(String nombre, String apellido, int edad){
+        
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;                
-        carrera.nombre = nombreCarrera;
-    }
-
-    //sobrecarga del constructor
-    public Persona(String nombre, String apellido, int edad, String nombreCarrera){
-        carrera = new Carrera(nombreCarrera);
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;                
-    }
+        
+    }    
 
     //Metodos (comportamiento/acciones de un objeto)
     public String darNombreCompleto(){
-        return nombre + " " + apellido;
+        return this.nombre + " " + this.apellido;
     }
     
     public String enviarSaludo(String saludado){
         if(edad > 40) return "Buenos dias, querido " + saludado;
         return "Hola, ¿como estas " + saludado + "?";
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    
 }
